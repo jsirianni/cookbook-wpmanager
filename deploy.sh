ceca6d736378c7dce3c704d4d1196e54b68d8574
jsirianni <jsirianni@ws1.sir.local>
2018-03-05 16:25:24 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ echo "cookbook_path '$HOME'" > $HOME/.chef/knife.rb
 echo "cookbook_path '$HOME'" > $HOME/solo.rb
 echo "{\"wp\":{\"sites\":[\"example.com\",\"example2.com\"]},\"run_list\":[\"recipe[wpmanager::default]\"]}" > dna.json
 nano dna.json
-sudo chef-solo -c cookbook-wpmanager/solo.rb -j cookbook-wpmanager/dna.json
+sudo chef-solo -c solo.rb -j dna.json
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name 'cookbook-wpmanager'
+name 'wpmanager'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures cookbook-wpmanager'
-long_description 'Installs/Configures cookbook-wpmanager'
+description 'Installs/Configures wpmanager'
+long_description 'Installs/Configures wpmanager'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
