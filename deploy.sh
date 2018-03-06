45a81d2eca3767b97403507b066855a96b6b6545
jsirianni <jsirianni@ws1.sir.local>
2018-03-05 20:54:56 -0500

    remove alert from example

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ curl -L https://www.opscode.com/chef/install.sh | sudo bash
 mkdir ~/.chef
 echo "cookbook_path '$HOME'" > $HOME/.chef/knife.rb
 echo "cookbook_path '$HOME'" > $HOME/solo.rb
-echo "{\"wp\":{\"sites\":[\"example.com\",\"example2.com\"],\"mysql\":{\"password\": \"password\"},\"alert\"{\"server\": \"\", \"account\":\"\", \"email\":\"\"}},\"run_list\":[\"recipe[wpmanager::default]\"]}" > dna.json
+echo "{\"wp\":{\"sites\":[\"example.com\",\"example2.com\"],\"mysql\":{\"password\": \"password\"}},\"run_list\":[\"recipe[wpmanager::default]\"]}" > dna.json
 nano dna.json
 clear
 echo "make sure dna.json includes your database root password"
