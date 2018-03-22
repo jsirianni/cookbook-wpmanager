81680188d64407f95fba9696f972708e87f27314
jsirianni <jsirianni@ws1.sir.local>
2018-03-21 20:26:06 -0400

    dont make example dna

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,6 @@ curl -L https://www.opscode.com/chef/install.sh | sudo bash
 mkdir ~/.chef
 echo "cookbook_path '$HOME'" > $HOME/.chef/knife.rb
 echo "cookbook_path '$HOME'" > $HOME/solo.rb
-echo "{\"wp\":{\"sites\":[\"example.com\",\"example2.com\"],\"mysql\":{\"password\": \"password\"}},\"run_list\":[\"recipe[wpmanager::default]\"]}" > dna.json
-nano dna.json
 clear
 echo "make sure dna.json includes your database root password"
-echo "Run: 'sudo chef-solo -c solo.rb -j dna.json'"
+echo "Run: 'sudo chef-solo -c solo.rb -j <dna.json>'"
