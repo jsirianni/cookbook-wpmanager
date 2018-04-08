252997366b8835674de2e39c16deb4bc3080f851
jsirianni <jsirianni@ws1.sir.local>
2018-04-07 21:18:38 -0400

    fix upload issue, roll back aggressive sec. policy

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,6 @@
 include_recipe 'wpmanager::firewall'
 include_recipe 'wpmanager::packages'
+include_recipe 'wpmanager::php'
 include_recipe 'wpmanager::nginx'
 include_recipe 'wpmanager::wordpress'
 include_recipe 'wpmanager::mysql'
